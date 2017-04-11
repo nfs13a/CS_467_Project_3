70e0357db76b3e2280b3d75df13341a08531e08f
nfs13a <nfs13a@acu.edu>
2017-04-11 15:29:09 -0500

    added to algorithm, code

--- implementation/src/code/NoughtsAndCrosses.java
+++ implementation/src/code/NoughtsAndCrosses.java
@@ -1,8 +1,14 @@
 package code;
 
+import java.util.Map;
+import java.util.Stack;
+import java.util.Vector;
+
 class Agent {
 	
-	protected int learningFactor;
+	protected int learningFactor;	//change to alter degree to which game results propagate down chosen states
+	protected Map<String, Integer> states;	//all state,value pairs known
+	protected Stack<String> chosenMoves;	//all states chosen in a game
 	
 	protected Agent() {
 		
@@ -26,7 +32,26 @@ class CrossAgent extends Agent {
  */
 public class NoughtsAndCrosses {
 	
+	private final int allWinStates[][] = {
+			{0,1,2},
+			{3,4,5},
+			{6,7,8},
+			{0,3,6},
+			{1,4,7},
+			{2,5,8},
+			{0,4,8},
+			{2,4,6}
+	};
+	
 	private boolean checkForCompletion(String gameState) {
+		if (!gameState.contains(".")) {	//if board is full
+			return true;
+		}
+		for (int i = 0; i < 8; i++) {
+			if (gameState.charAt(allWinStates[i][0]) == gameState.charAt(allWinStates[i][1]) && gameState.charAt(allWinStates[i][1]) == gameState.charAt(allWinStates[i][2])) {	//if player has won
+				return true;
+			}
+		}
 		return false;
 	}
 
